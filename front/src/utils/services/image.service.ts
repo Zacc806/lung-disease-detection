7fc3d8481287a6d98a59a2f0fa81fe79cf3b52ba
Damir <damir@myterior.kz>
2024-10-19 19:06:01 +0500

    service for uploading image

--- front/src/utils/services/image.service.ts
+++ front/src/utils/services/image.service.ts
@@ -1,6 +1,6 @@
 import $host from './axios'
 
-export const uploadImage = async (body: unknown) => {
+export const uploadImage = async (body: FormData) => {
     const { data } = await $host.post('image', body, {
         headers: {
             'Content-Type': 'multipart/form-data',
